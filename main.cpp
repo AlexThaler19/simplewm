d4102cf388ff7eecd3e12f18790fb3ac890685dd
alex <alex.thaler@outlook.it>
2024-02-26 10:40:08 +0100

    main ohne glog

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <cstdlib>
-#include <glog/logging.h>
+//#include <glog/logging.h>
 #include "window_manager.h"
 
 using ::std::unique_ptr;
 
 int main(int argc, char** argv) {
-    ::google::InitGoogleLogging(argv[0]);
+    //::google::InitGoogleLogging(argv[0]);
 
     unique_ptr<WindowManager> window_manager(WindowManager::Create());
     if (!window_manager) {
-        LOG(ERROR) << "Failed to initialize window manager";
+        //LOG(ERROR) << "Failed to initialize window manager";
         return EXIT_FAILURE;
     }
 
